ca4f95643836e809e85d8ec52b742da074fccbf5
artem <bam10112002@gmail.com>
2022-10-31 13:40:43 +0300

    add select into travel agency table

---
+++ 02_insert_into_travel_agency.sql
@@ -0,0 +1,4 @@
+INSERT into travel_agency(id, name, inn) values (1, 'sun travel', '123456734');
+INSERT into travel_agency(id, name, inn) values (2, 'sea travel', '324653245');
+INSERT into travel_agency(id, name, inn) values (3, 'free travel', '324653232');
+INSERT into travel_agency(id, name, inn) values (4, 'i don`t know travel', '111353245');
