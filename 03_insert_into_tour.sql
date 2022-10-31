a88255824242a6778fc9f37d3cc0afae20025d03
artem <bam10112002@gmail.com>
2022-10-31 13:41:28 +0300

    add insert into tour table

---
+++ 03_insert_into_tour.sql
@@ -0,0 +1,15 @@
+INSERT into tour (id, name, price, description, start_date, flight_time, travel_agency_id)
+values (1, 'good day', 999, 'lorem ipsum dolor sit amet, consectetur adip', '2020-05-04' , 8, 1);
+INSERT into countries_tour (country_id, tour_id) values (1, 2);
+
+INSERT into tour (id, name, price, description, start_date, flight_time, travel_agency_id)
+values (2, 'London riot', 2450, 'lorem ipsum dolor sit amet, consectetur adip', '2008-11-11', 14, 1);
+INSERT into countries_tour (country_id, tour_id) values (2, 1);
+
+INSERT into tour (id, name, price, description, start_date, flight_time, travel_agency_id)
+values (3, 'Some tour', 1490, 'lorem ipsum dolor sit amet, consectetur adip','2020-05-04', 11, 1);
+INSERT into countries_tour (country_id, tour_id) values (3, 2);
+
+INSERT into tour (id, name, price, description, start_date, flight_time, travel_agency_id)
+values (4, 'Tour 2', 5020, 'lorem ipsum dolor sit amet, consectetur adip', '2008-5-11',9, 1);
+INSERT into countries_tour (country_id, tour_id) values (4, 3);
