5c6d1ab4de851ae7efd6c965869bcadb7fdc5753
artem <bam10112002@gmail.com>
2022-10-31 13:39:48 +0300

    add select into countries

---
+++ 01_insert_into_countries.sql
@@ -0,0 +1,3 @@
+INSERT into countries(name, language) values ('England', 'English');
+INSERT into countries(name, language) values ('Brazil', 'Portuguese');
+INSERT into countries(name, language) values ('Russian Federation', 'Russian');
